ef99b9f2f03b4b572d4e8c46cbeb07f7e17be0b5
suraj singh <surajthakurrs45@gmail.com>
2023-08-10 20:05:51 +0530

    Fahrenheit to Celsius

---
+++ Basic Problem/FahrenheitToCelsius.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class FahrenheitToCelsius{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter Temperature In Fahrenheit to Convert In Celsius");
+float Fahrenheit=sc.nextFloat();
+float Celsius= ((Fahrenheit-32)*5/9);
+
+System.out.println("Fahrenheit \u00B0F "+Fahrenheit+" to Celsius Is "+Celsius+" \u00B0C");
+}
+}
