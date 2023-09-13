fc216abdc36ceebe5167938b1ffefb8a68aa224e
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-13 19:24:59 +0530

    initialize array with 1

---
+++ array/demo.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class demo {
+
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+Arrays.fill(arr,  1);
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
+    }
+}
