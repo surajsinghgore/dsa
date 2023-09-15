6af765b6400b80df04696d8d8419b80167e741a3
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-15 20:03:51 +0530

    swap alternate in array

---
+++ array/swapalternate.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class swapalternate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // taking array size from user
+        System.out.print("Enter the size of Array : ");
+        int size = sc.nextInt();
+        // initializing array
+        int arr[] = new int[size];
+        // taking input from user in array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Arr [ " + i + " ] = ");
+            arr[i] = sc.nextInt();
+        }
+
+        // printing original array
+        System.out.println("\nOriginal Array  Before Extract Odd And Even Element From Array  ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        // Swap Alternative Logic
+    
+
+        for (int i = 0; i < arr.length-1; i+=2) {
+            // swap values in array
+           int temp=arr[i];
+           arr[i]=arr[i+1];
+           arr[i+1]=temp;
+        }
+
+        
+         // array after alternate swap array
+        System.out.println("\nArray After Alternate Swap");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
