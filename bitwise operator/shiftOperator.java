b0e88b8bf4fec4ba2ffbe63ccf7901decd1ff97e
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-21 17:24:51 +0530

    shift operator

---
+++ bitwise operator/shiftOperator.java
@@ -0,0 +1,8 @@
+public class shiftOperator {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        System.out.println("a^b = ");
+        System.out.println(a^b);
+    }
+}
