964713cf7e6cc2a83a4e1ee3b4a1be718f2bf488
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-21 09:37:52 +0530

    bitwise not operator

---
+++ bitwise operator/notOperator.java
@@ -0,0 +1,12 @@
+public class notOperator {
+    
+    public static void main(String[] args) {
+        
+        int a=2;
+        int b=5;
+        System.out.print(" ! a  = ");
+        System.out.println(~a);
+        System.out.print(" !b  = ");
+        System.out.println(~b);
+    }
+}
