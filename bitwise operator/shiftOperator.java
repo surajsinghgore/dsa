86a8f7b522cdeeba4879f609aaf45b729b8a2d53
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-21 17:31:35 +0530

    xor opertor

--- bitwise operator/shiftOperator.java
+++ bitwise operator/shiftOperator.java
@@ -1,8 +1,16 @@
 public class shiftOperator {
     public static void main(String[] args) {
-        int a=2;
-        int b=3;
-        System.out.println("a^b = ");
-        System.out.println(a^b);
+        int a = 5;
+        int b = 3;
+        int c = 4;
+        int d = 6;
+        System.out.println("a>>1");
+        System.out.println(a>>1);
+        System.out.println("b>>2");
+        System.out.println(b>>2);
+        System.out.println("c<<1");
+        System.out.println(c<<1);
+        System.out.println("d<<2");
+        System.out.println(d<<2);
     }
 }
