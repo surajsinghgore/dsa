514eb240bf869656fc3872633cee5454b02c50cd
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-21 09:21:06 +0530

    bitwise and operator

---
+++ bitwise operator/andOperator.java
@@ -0,0 +1,11 @@
+
+public class andOperator {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        System.out.print("a and b = ");
+        System.out.println(a & b);
+        System.out.print("b and a = ");
+        System.out.println(b & a);
+    }
+}
