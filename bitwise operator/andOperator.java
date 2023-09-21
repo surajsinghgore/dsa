83efa958f6475a1a9272b1e02a703d7c90ffdbd0
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-21 09:26:38 +0530

    bitwise or operator

--- bitwise operator/andOperator.java
+++ bitwise operator/andOperator.java
@@ -1,8 +1,8 @@
 
 public class andOperator {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 2;
+        int a = 5;
+        int b = 6;
         System.out.print("a and b = ");
         System.out.println(a & b);
         System.out.print("b and a = ");
---
+++ bitwise operator/orOperator.java
@@ -0,0 +1,10 @@
+public class orOperator {
+    public static void main(String[] args) {
+        int a=9;
+        int b=5;
+        System.out.print("a or b = ");
+        System.out.println(a|b);
+        System.out.print("b or a = ");
+        System.out.println(b|a);
+    }
+}
